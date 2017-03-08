e9f768a5215e27eca2749b40e05504299a6bbe83
aruntk <aruntknambiar@gmail.com>
2017-03-08 11:04:02 +0530

    removed unnecessary callback

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'sar:meteor-activedirectory',
-  version: '0.0.6',
+  version: '0.0.7',
   // Brief, one-line summary of the package.
   summary: 'This package uses active directory for Authentication using LDAP',
   // URL to the Git repository containing the source code for this package.
