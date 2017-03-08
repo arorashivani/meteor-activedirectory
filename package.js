98d27808bad8d4afb9ea75e30e11e747ecd9bfc8
aruntk <aruntknambiar@gmail.com>
2017-03-08 11:50:53 +0530

    minor

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 
-  version: '0.0.8',
+  version: '0.0.9',
 
   // Brief, one-line summary of the package.
   summary: 'This package uses active directory for Authentication using LDAP',
@@ -20,12 +20,12 @@ Package.onUse(function(api) {
   api.versionsFrom('1.4.2');
   api.use('ecmascript');
   api.use('underscore');
-//  api.mainModule('meteor-activedirectory.js');
- api.use([ 'tinytest'], ['client', 'server']);
- //api.add_files('meteor-file-test.js', ['client', 'server']);
- api.add_files("ad_server.js", "server");
- api.add_files("ad_client.js", "client");
- api.use([
+  //  api.mainModule('meteor-activedirectory.js');
+  api.use([ 'tinytest'], ['client', 'server']);
+  //api.add_files('meteor-file-test.js', ['client', 'server']);
+  api.add_files("ad_server.js", "server");
+  api.add_files("ad_client.js", "client");
+  api.use([
     "accounts-base",
     "check"
   ], ["client", "server"]);
@@ -42,5 +42,5 @@ Package.onTest(function(api) {
   //api.use('meteor-activedirectory');
 
   api.add_files('meteor-file-test.js', ['client', 'server']);
-//  api.mainModule('meteor-activedirectory-tests.js');
+  //  api.mainModule('meteor-activedirectory-tests.js');
 });
