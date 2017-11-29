75197d7ff8b588a8ada81a1e420435492b545059
aruntk <aruntknambiar@gmail.com>
2017-11-29 08:57:33 +0530

    0.0.11

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'sar:meteor-activedirectory',
-  version: '0.0.10',
+  version: '0.0.11',
   // Brief, one-line summary of the package.
   summary: 'This package uses active directory for Authentication using LDAP',
   // URL to the Git repository containing the source code for this package.
